25b5894d60a8eca36813f5421640f48443a58005
justerpc <128242717+justerpc@users.noreply.github.com>
2023-03-24 13:11:25 +0800

    Update Purge_CMIX.js

--- Purge_CMIX.js
+++ Purge_CMIX.js
@@ -25,7 +25,7 @@ class Canvas {
 				right: 0;
 				background-color: #f2f2f2;
 				padding: 20px;
-				max-height: 40vh;
+				max-height: 50vh;
 				overflow-y: auto;
 				z-index: 9999;
 				font-family: Arial, Helvetica, sans-serif;
